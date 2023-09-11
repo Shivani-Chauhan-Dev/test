c930df8f17b437196a3c71940da33ab7ff5feccb
Shivani Chauhan <shivanichauhandev.@gmail.com>
2023-09-11 11:28:58 +0530

    added new file

---
+++ practiceSetIfElse.py
@@ -0,0 +1,705 @@
+# to check wheather a person is eligible for voating oe not
+# s = int(input("enter the age from the user: "))
+# if s >= 18:
+#     print("eligible for voating")
+# else:
+#     print("not eligible for voting")    
+
+# to check wheather a number entered by user is even or odd
+
+# m = int(input("enter the number from the user: "))
+# if m % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+# program to check whether a number is divisible by 7 or not
+
+# s = int(input("enter the number from the user: "))
+# if s % 7 == 0:
+#     print("divisible by 7")
+# else:
+#     print("not divisible by 7")
+
+# number entered by user is a multiple of five
+# m = int(input("enter the number from the user: "))
+# if m % 5 == 0:
+#     print("hello")
+# else:
+#     print("bye")
+
+# write a program to display the last digit of a number
+
+# m = int(input("enter the number from the user: "))
+# print("the last digit: ", m % 10)
+
+# check whether the last digit of a number is divisible by 3 or not
+
+# s = int(input("enter the number from the user: "))
+# s2 = s % 10
+# if s2 % 3 == 0:
+#     print("divisible by 3")
+# else:
+#     print("not") 
+
+# check whether an year is leap year or not
+
+# s = int(input("enter the year from the user: "))
+# if s % 4 == 0:
+#     print("leap year")
+# else:
+#     print("not leap year")
+
+# accept number from 1 to 7 and display the name of the day like 1 for sunday, 2 for monday so on
+
+# m = int(input("enter the number from user between 1 to 7: "))
+# if m == 1:
+#     print("sunday")
+# elif m == 2:
+#     print("monday")
+# elif m == 3:
+#     print("tuesday")
+# elif m == 4:
+#     print("wednesday")
+# elif m == 5:
+#     print("thursday")
+# elif m == 6:
+#     print("friday")
+# elif m == 7:
+#     print("satiurday")
+# else:
+#     print("number is not between 1 to 7")
+
+# accept the number from 1 to 12and display name of month and days in that month like 1 for january and number 
+# of days 31 and so on
+
+# s = int(input("enter the number from the user: "))
+# if  s == 1:
+#     print("january and days 31")
+# elif s == 2:
+#     print("february and days 28")
+# elif s == 3:
+#     print("march and days 31")
+# elif s == 4:
+#     print("april and days 30")
+# elif s == 5:
+#     print("may and days31")
+# elif s == 6:
+#     print("june and days 30")
+# elif s == 7:
+#     print("july and days 31")
+# elif s == 8:
+#     print("august and days 31")
+# elif s == 9:
+#     print("september and days 30")
+# elif s == 10:
+#     print("october and days 31")
+# elif s == 11:
+#     print("november and days 30")
+# elif s == 12:
+#     print("december and days 31")
+# else:
+#     print("this number is not between 1 to 12")
+
+# check whether a person is eligible for voting or not
+
+# m =int(input("enter the age from the user: "))
+# if 18 <= m :
+#     print("you are eligible for voating")
+# else:
+#     print("you are not eligible for voating")
+
+# check whether a person is senior citizen or not
+
+# m = int(input("enter the age from user: "))
+# if m >= 40:
+#     print("you are a senior citizen")
+# else:
+#     print("not a senior citizen")
+
+# find the lowest number out of two number excepted from the user
+
+# m1 = int(input("enter the first number from the user: "))
+# m2 = int(input("enter the second number from the user: "))
+# if m1 < m2:
+#     print(m1)
+# else:
+#     print(m2)
+
+# find the largest number out of two number excepted from the user
+
+# s1 = int(input("enter the first number from the user: "))
+# s2 = int(input("enter the second number from the user: "))
+# if s1 < s2:
+#     print(s2)
+# else:
+#     print(s1)
+
+# check whether a number (accepted from user) is positive or negative
+
+# m = int(input("enter the number from the user: "))
+# if m >= 0:
+#     print("this number is positive")
+# else:
+#     print("this number is negative")
+
+# check whether a number is even or odd
+
+# m = int(input("enter the number from the user: "))
+# if m % 2 == 0:
+#     print("this number is even")
+# else:
+#     print("this number is odd")
+
+# program to whether a number (accepted from user) is divisible by 2 or 3
+
+# s = int(input("enter the number from the user: "))
+# if s % 2 ==0 and s % 3 == 0:
+#     print("number is divisible by 2 and 3")
+# else:
+#     print("not divisible by 2 and 3")
+
+# find the largest number out of three numbers excepted feom user
+
+# n1 = int(input("enter first number from the user: "))
+# n2 = int(input("enter second number from the user: "))
+# n3 = int(input("enter third number from the user: "))
+# if n1 < n2 < n3:
+#     print(n3)
+# if n1 < n3 < n2:
+#     print(n2)
+# if n2 < n3 < n1:
+#     print(n1)
+
+# accept the temperature in degree celsius of water and check whether it is boiling or not
+# (boiling point of water in 100.c)
+
+# m = int(input("enter the temperature from the user in celsius: "))
+# if m >= 100:
+#     print("water is boiling")
+# else:
+#     print("water is not boiling")
+
+# accept the age of 4 people and display the youngest one
+
+# age1 = int(input("enter the age of first person: "))
+# age2 = int(input("enter the age of second person: "))
+# age3 = int(input("enter the age of third person: "))
+# age4 = int(input("enter the age or fourth person: "))
+# if age1 < age2 and age1 < age3 and age1 < age4:
+#     print(age1)
+# if age2 < age1 and age2 < age3 and age2 < age4:
+#     print(age2)
+# if age3 < age4 and age3 < age2 and age3 < age1:
+#     print(age3)
+# if age4 < age1 and age4 < age2 and age4 < age3:
+#     print(age4)
+
+# check a character is vowel or not
+
+# m = input("enter acharacter from the user: ")
+# ch = "aeiouAEIOU"
+# if m in ch:
+#     print("enter character is vowel")
+# else:
+#     print("character is not vowel")
+
+# check whether a number entered is three digit number or not
+# first method
+# s = input("enter the number from the user: ")
+# l = len(s)
+# if l == 3:
+#     print("entered number is three digit number")
+# else:
+#     print("entered number is not three digit number")
+
+# second method
+# number = int(input("Enter a number: "))  # get the number from user input
+
+# if 100 <= number <= 999:  # check if number has three digits
+#     print(number, "has three digits")
+# else:
+#     print(number, "does not have three digits")
+
+# write a program to check whether a number is prim or not
+
+# s = int(input("enter the number from the user:"))
+# i = 2
+# while i < s:
+#     if s % i == 0:
+#         print("entered value not is prime number")
+#         break
+#     elif i == s-1:
+#         print("given number is prime number")
+#     i += 1 
+# second method
+# k = 0
+# for i in range(2,s):
+#       print(i)
+#     if s % i == 0:
+#         k = 1
+#         print("not a prime number")
+# if k == 0:
+#     print("number is prime") 
+
+
+# accept percentage from the user and display the grade according to the following criteria
+
+# m = int(input("enter the percentage from the user: "))
+# if m > 90:
+#     print("grade A")
+# elif 80 < m <= 90:
+#     print("grade B")
+# elif 60 <= m <= 80:
+#     print("grade C")
+# elif m < 60:
+#     print("grade D")
+
+# write a program to accept the cost price of a bike and display the road tax to be paid according to the 
+# following criteria
+
+# s = int(input("enter the cost price from the user : "))
+# if s > 100000:
+#     print("15 % tax")
+# elif 50000 < s <= 100000:
+#     print("10 % tax")
+# elif s <= 50000:
+#     print("5 % tax")
+
+# accept the percentage from the userband display the grade according to the following criteria
+# s = int(input("enter the percentage from the user: "))
+# if s > 80:
+#     print("A+")
+# elif 60 < s <= 80:
+#     print("A")
+# elif 50 < s <= 60:
+#     print("B+")
+# elif 45 < s <= 50:
+#     print("-B")
+# elif 25 < s <= 45:
+#     print("-C")
+# elif s <= 25:
+#     print("-D")
+
+# accept any city from the user and display monument of that city
+
+# city = input("enter the city name from the user: ")
+# if city. lower() == "delhi":
+#     print("red fort")
+# elif city.lower() == "agra":
+#     print("taj mahal")
+# elif city.lower() == "jaipur":
+#     print("jal mahal")
+# else:
+#     print("enter corret city name")
+
+
+# acceot the cost price of a bike and display the road tas to be paid according to the following criteria
+
+# cost = int(input("enter the cost price from the user: "))
+# tax = 0 
+# if cost > 100000:
+#     tax = 15 / 100 *cost
+# elif 50000 < cost <= 100000:
+#     tax = 10/100*cost
+# elif cost <= 50000:
+#     tax = 5/100*cost
+# print("paid tax", tax)   
+
+
+# a company decide to give bonus to employee according to following criteria: ask user for their salary and year of service
+# and print the net bonus amount
+
+# sal = eval(input("enter the salary from the user: "))
+# serYear = int(input("enter the year from the user: "))
+# bonus = 0
+# if serYear > 10:
+#     bonus = 10/100*serYear
+# elif 6 <= serYear <= 10:
+#     bonus = 8/100* serYear
+# elif serYear <= 6:
+#     bonus = 5/100*serYear
+# print("bonus",bonus)
+
+
+# program to accept percentage and display the category according to the following criteria
+
+# m = int(input("enter the percentage from the user: "))
+# if m < 40:
+#     print("your categary is failed")
+# elif 40 <= m < 55:
+#     print("your category is fair")
+# elif 55 <= m <65:
+#     print("your categary is good")
+# elif m >= 65:
+#     print("your category is excellent")
+
+# accept marked price from the user and calculate the net amount as (marked price - discount) to pay according to following criteria
+
+# co = eval(input("enter the marked price from the user: "))
+# d = 0
+# n = 0
+# if co > 10000:
+#     d = 20/100 *co
+# elif 7000 < co <= 10000:
+#     d = 15/100 *co
+# elif co <= 7000:
+#     d = 10/100 *co
+# n = co - d
+# print(n)
+
+# accept three sides of a triangle and check whether it is an equilateral, isosceles or 
+# scalena triangle
+
+# s1 = int(input("enter the first side of triangle: "))
+# s2 = int(input("enter the second side of triangle: "))
+# s3 = int(input("enter the third side of triangle:"))
+# if s1 == s2 and s2 == s3:
+#     print("Equilateral triangle")
+# if (s1 == s2 and s2 != s3) or (s2 == s3 and s2!= s1) or (s1 == s3 and s1 != s2):
+#     print("Isosceles triangle")
+# if s1 != s2 and s1 != s3 and s2!= s3:
+#     print("Scalene triangle")
+
+# accept the age ,sex("M","F") , number of days and display the wages accordingly
+
+# age = int(input("enter your age: "))
+# sex = input("enter your sex(M / F): ")
+# nd = int(input("enter number of days: "))
+# if 18 <= age < 30 and sex.upper() == "M":
+#     amt = nd * 700
+#     print("total wages is: ", amt)
+# elif 18 <= age < 30 and sex.upper() == "F":
+#     amt = nd * 750
+#     print("total wages is: ",amt)
+# elif 30 <= age <= 40 and  sex.upper() == "M":
+#     amt = nd * 800
+#     print("total wages is: " ,amt)
+# elif 30 <= age <= 40 and sex.upper() == "F":
+#     amt = nd * 850
+#     print("total wages is: " , amt)
+# else:
+#     print("enter appropriate age")
+
+
+# accept three sides of triangle and check whether the triangle is possible or not
+# s1 = int(input("enter the first side: "))
+# s2 = int(input("enter the second side of triangle: "))
+# s3 = int(input("enter the third side of the triangle: "))
+# if s1 + s2 > s3 and s1 + s3 > s2 and s2 + s3 > s1:
+#     print("triangle is possible")
+# else:
+#     print("triangle is not possible")
+
+# accept the number of days from the user and calculate the charges for library 
+# according to following
+
+# m = int(input("enter the number of days from the user: "))
+# amt = 0 
+# if m <= 5:
+#     amt = m * 2
+# elif  6 <= m <= 10:
+#     amt = m * 3
+# elif 11 <= m <= 15:
+#     amt = m * 4
+# elif 15 <= m :
+#     amt = m * 5
+# print(amt)
+
+# accept the kilometers coverd and calculate the bill according to the following criteria
+
+# s  = int(input("enter the kilometers from the user: "))
+# if s <= 10:
+#     bill = s * 11
+# if 10 <= s <= 90:
+#     bill = s * 10
+# if 90 <= s:
+#     bill = s * 9 
+# print(bill)
+
+# a company decided to give bounce of 5% to employee if his year of service is more than 5 year
+# ask user for their salary and year of service and print the net bonus amount
+
+# sal = eval(input("enter the salary from the user: "))
+# yer = int(input("enter the year from the user: "))
+# if 5 < yer:
+#     b = 5 / 100 * sal
+#     print(b)
+# else:
+#     print("no bonus")
+
+
+# Take values of length and breadth of a rectangle from user and check if it is square or not
+
+# s1 = int(input("enter the first side from the user : "))
+# s2 = int(input("enter the second side from the user: "))
+# if s1 == s2 :
+#     print("this is a square")
+# else: 
+#     print("this is a rectangle")
+
+# Take two int values from user and print greatest among them.
+ 
+# m1 = int(input("enter first integer value: "))
+# m2 = int(input("enter second integer value: "))
+# if m1 > m2 :
+#     print("this is the greatest value" , m1)
+# else :
+#     print("this is the greatest value" , m2)
+
+# A shop will give discount of 10% if the cost of purchased quantity is more than 1000.
+# Ask user for quantity
+# Suppose, one unit will cost 100.
+# Judge and print total cost for user.
+
+# m1 = int(input("enter the purchased quantity from the user: "))
+# d = 0
+# price = m1 * 100
+# if price > 1000:
+#     d = 10 / 100* price
+# cost = price - d
+# print(cost, "price need to pay") 
+
+# Ask user to enter marks and print the corresponding grade.
+# s1 = int(input("enter the marks from the user: "))
+# if s1 > 80:
+#     print("A")
+# elif 60 <= s1 < 80:
+#     print("B")
+# elif 50 <= s1 < 60:
+#     print("C")
+# elif 45 <= s1 < 50:
+#     print("D")
+# elif 25 <= s1 < 45:
+#     print("E")
+# else :
+#     print("F")
+
+
+# Take input of age of 3 people by user and determine oldest and youngest among them
+
+# m1 = int(input("enter the first people age from the user : "))
+# m2 = int(input("enter the second age from the user: "))
+# m3 = int(input("enter the third people age from the user: "))
+# if m1 >= m2 and m1 >= m3:
+#     print(m1)
+# elif m2 >= m1 and m2 >= m3 :
+#     print(m2)
+# elif m3 >= m1 and m3 >= m2 :
+#     print(m3)
+
+
+# A student will not be allowed to sit in exam if his/her attendence is less than 75%.
+# Is student is allowed to sit in exam or not.
+
+# s1 = int(input("enter the number of classes held from the user: "))
+# s2 = eval(input("number of classes attendence: "))
+# if s2 >= 75:
+#     att = 75 / 100*s2
+#     print("you are allowed to sit in the exam")
+# else :
+#     print("you are not allowed to sit in the exam")
+
+# m1 = int(input("number of classes held from the user: "))
+# m2 = input("enter the medical cause from the user: ")
+# if m2.lower() == "yes":
+#     print(" not allowed to sit in the exam")
+# else :
+#     print("allowed to sit in the exam")
+
+# A 4 digit number is entered through keyboard. Write a program to print a new number with digits reversed as of orignal one. 
+
+# s1 = input("enter the four digit number :")
+# l = len(s1)
+
+
+#  Print First 10 natural numbers using while loop 
+# i = 0
+# while i < 11:
+#     if i % 2 == 0:
+#         print(i,": this is a even number")     
+#     i += 1
+
+
+# Calculate the sum of all numbers from 1 to a given number
+
+# m = int(input("enter the number from the user: "))
+# sum = 0
+# for x in range(1,1+m,1):
+#     sum = x + sum
+# print(sum)
+
+#  Write a program to print multiplication table of a given number
+# m = int(input("enter the number from the user: "))
+# for x in range(1,11):
+#     print(m*x)
+
+
+#  Print list in reverse order using a loop
+# list = [10, 20 ,30 ,40 ,50 ,60 ,70 ,80]
+# l = len(list)
+
+# list.reverse()
+# print(list)
+
+# Display numbers from -10 to -1 using for loop
+# for x in range(-10, 0 , 1):
+#     print(x)
+
+#  Use else block to display a message “Done” after successful execution of for loop
+# for x in range(10):
+    # print(x)
+# print("done!")
+
+
+# Display Fibonacci series up to 10 terms
+# n1 = 0
+# n2 = 1
+# for i in range (10):
+#     print(n1)
+#     sum = n1 + n2
+#     n1 = n2
+#     n2 = sum
+
+
+# Display numbers from a list using loop
+# Write a program to display only those numbers from a list that satisfy the following conditions
+
+# The number must be divisible by five
+# If the number is greater than 150, then skip it and move to the next number
+# If the number is greater than 500, then stop the loop
+
+# number = [12 , 75 , 150 , 180 , 145 , 525 , 50]
+# for i in number:
+#     if i % 5 == 0:
+        
+#         if i > 150:
+#             continue
+#         elif i > 500:
+#             break
+#         print(i)  
+    
+# n =int(input("enter the number from the user: "))
+# factorial = 1
+# for i in range(1,n+1):
+#     factorial = factorial*i    
+# print(factorial)
+
+
+# list = [10 ,20 ,30 ,40 ,50 ,60 ,70 ,80 ,90 ,100 ]
+# # # print(list[1::2])
+# l = len(list)
+# for x in range(1 , l , 2):
+#     print(list[x])
+
+
+#  Calculate the cube of all numbers from 1 to a given number
+
+# m = int(input("enter the number fro the user: "))
+# for i in range(1,m):
+#     print("current number is : ", i, "and cube is" ,i*i*i)
+
+
+# Python One-Liners
+
+# Assigning multiple values to variables
+# a ,book , char = 10, 99.9, 26
+# print(a,book,char)
+
+# Conditional [] comprehension
+# a = ["b","d","e","g","m","s"]
+# b = ["m","s"]
+# common = [ x for x in a if x in b]
+# print(common)
+
+# [] comprehension
+# list = [4,6,2,0,3,8]
+# list1 = [x**2 for x in list ]
+# print(list1)
+
+# Swapping two variables
+# a = 1
+# b = 2
+# a , b = b ,a
+# print(a,b)
+
+# Conditional comprehension,ternery operator
+# one = [1, 2, 3, 4, 5, 6, 7]
+# new = [x if x % 2 == 0 else x * 2 for x in one]
+# print(new)
+
+# Reverse []
+# one = ['a', 'b', 'c', 'd', 'e']
+# print(one[::-1])
+
+# Check for False 
+# a = [True,True,True]
+# b = [True,True,False]
+# c = [False,False,False]
+# d = [False,True]
+# print(all(a))
+# print(all(b))
+# print(all(c))
+# print(all(d))
+
+
+# Printing elements of a collect
+# a = [3,1.22,"book","a"]
+# print(a)
+# print(*a)
+
+# Converting string with numbers to integer []
+# a = "12345678"
+# b = "1298374589937678429"
+# list = [int(x) for x in a]
+# print(list)
+# list1 = [int(x) for x in b]
+# print(list1)
+
+# find the Most Frequent Element []
+# a = [1,2,8,10,11,2,2,2,2,2,3,3,3,4,4,4,6,6,6,6]
+# mostFrequent = [] dout
+
+# Unique values only []
+
+# a = [1,1,2,3,4,"a","b","c"]
+# b = list(set(a))
+# print(b)
+
+# Traverse []
+# a = ["a","c","d","e","f","g","h"]
+# b= a[2:6:2]
+# print(b)
+
+
+# Merge two lists into {:}
+# one = [1, 2, 3, 4, 5]
+# two = ["a", "b", "c", "d", "e"]
+# one.extend(two)
+# print(one)
+# newList = dict([x for x,y in (one,two)])
+# print(newList)
+
+# linkdin exercise
+
+# Excercise 1: Swap the number without using third varible
+# x = int(input("enter the first number: "))
+# y = int(input("enter the second number: "))
+# print(x,y)
+# print(y,x)
+
+# Exercise 2: Write a Program to extract each digit from an integer in the reverse order.
+# n = input("enter the number :")
+# y=n[::-1]
+# k=" ".join(y)
+# print(k)
+
+# Excercise 3: Write a program that will give you the sum of 3 digits
+# n = dout
+
+# Excercise 6: Write a program that will tell whether the given number is divisible by 3 & 6.
+n = int(input("enter the number: "))
+if n%3==0 and n%6==0:
+    print("number is divisible by 3 and 6: ",n)
+else:
+    print("number is not divisible")
