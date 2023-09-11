09828a80ef9c691e6e85eae6f1597b8de0a01832
Shivani Chauhan <shivanichauhandev.@gmail.com>
2023-09-11 11:27:09 +0530

    adde new file

---
+++ patternExample.py
@@ -0,0 +1,431 @@
+# n = int(input("enter the number of row: "))
+# for i in range(n):
+#     print("*" * n)
+
+
+# n = int(input("enter the number of row: "))
+# for i in range(n):
+#     for j in range (i):
+#         print(i , end = "")
+#     print()
+
+
+# n = int(input("enter the number of row: "))
+# for i in range(1, 1+n):
+#     for j in range(1, 1+n):
+#         print(n+1-i,end = "")
+#     print()
+
+# n = int(input("enter the number of row: "))
+# for i in range(n):
+#     print(i)
+    # for j in range (i):
+    #     print(i, end = "")
+    # print()
+
+
+# n = int(input("enter the number of row : "))
+# for i in range(1, 1+n):
+#     for j in range(1, i+1):
+#         print(j, end = "")
+#     print()
+
+
+# row = int(input("enter the given number from the user: "))
+# for i in range (row):
+#     for j in range (i):
+#         print(j,end = " ")
+#     print("  ")
+
+# new question
+
+# star pattern
+# ****
+# ****
+# ****
+# ****
+
+# m = int(input("enter the number from the user: "))
+# for x in range(m):
+#     for y in range(m):
+#         print("*", end= "")
+#     print()
+
+
+# pattern
+# 1  
+# 2 2  
+# 3 3 3  
+# 4 4 4 4  
+# 5 5 5 5 5
+
+# m = int(input("enter number from the user: "))
+# for x in range(m):
+#     for y in range(x):
+#         print("*", end = "")
+#     print()
+
+# Pyramid pattern of numbers
+# 1 
+# 1 2 
+# 1 2 3 
+# 1 2 3 4 
+# 1 2 3 4 5
+
+# m = int(input("enter the number from the user: "))
+# for x in range(1,m+1):
+#     for y in range(1,x+1):
+#         print(y, end= "")
+#     print()
+
+# number pattern
+# 4444
+# 333
+# 22
+# 1
+
+# m = int(input("enter the number from the user: "))
+# for x in range(m, 0 ,-1):
+#     for y in range(x):
+#         print(x, end = "")
+#     print( )
+
+
+
+# Pattern-1: 
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+
+# n = int(input("enter number of row: "))
+# for i in range(n):
+#     print("*"*n)
+
+# Pattern-2: 
+# 1 1 1 1 1 1 1 1 1 1
+# 2 2 2 2 2 2 2 2 2 2
+# 3 3 3 3 3 3 3 3 3 3
+# 4 4 4 4 4 4 4 4 4 4
+# 5 5 5 5 5 5 5 5 5 5
+# 6 6 6 6 6 6 6 6 6 6
+# 7 7 7 7 7 7 7 7 7 7
+# 8 8 8 8 8 8 8 8 8 8
+# 9 9 9 9 9 9 9 9 9 9
+# 10 10 10 10 10 10 10 10 10 10
+
+# n = int(input("enter the number of row: "))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(i,end=" ")
+#     print()
+
+# pattern-3
+# 1 2 3 4 5 6 7 8 9 10
+# 1 2 3 4 5 6 7 8 9 10
+# 1 2 3 4 5 6 7 8 9 10
+# 1 2 3 4 5 6 7 8 9 10
+# 1 2 3 4 5 6 7 8 9 10
+# 1 2 3 4 5 6 7 8 9 10
+# 1 2 3 4 5 6 7 8 9 10
+# 1 2 3 4 5 6 7 8 9 10
+# 1 2 3 4 5 6 7 8 9 10
+# 1 2 3 4 5 6 7 8 9 10
+
+# n = int(input("enter the number of row: "))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(j,end = " ")
+#     print()
+
+
+# Pattern-6: 
+# 10 10 10 10 10 10 10 10 10 10
+# 9 9 9 9 9 9 9 9 9 9
+# 8 8 8 8 8 8 8 8 8 8
+# 7 7 7 7 7 7 7 7 7 7
+# 6 6 6 6 6 6 6 6 6 6
+# 5 5 5 5 5 5 5 5 5 5
+# 4 4 4 4 4 4 4 4 4 4
+# 3 3 3 3 3 3 3 3 3 3
+# 2 2 2 2 2 2 2 2 2 2
+# 1 1 1 1 1 1 1 1 1 1
+
+# n = int(input("enter the number of row: "))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(n+1-i,end = " ")
+#     print()
+
+# pattern-7
+# 10 9 8 7 6 5 4 3 2 1
+# 10 9 8 7 6 5 4 3 2 1
+# 10 9 8 7 6 5 4 3 2 1
+# 10 9 8 7 6 5 4 3 2 1
+# 10 9 8 7 6 5 4 3 2 1
+# 10 9 8 7 6 5 4 3 2 1
+# 10 9 8 7 6 5 4 3 2 1
+# 10 9 8 7 6 5 4 3 2 1
+# 10 9 8 7 6 5 4 3 2 1
+# 10 9 8 7 6 5 4 3 2 1
+
+# n = int(input("enter the number of row: "))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(n+1-j, end = " ")
+#     print()
+
+# Pattern-9:
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# * * * * * *
+# * * * * * * * 
+
+# n = int(input('enter the number of row: '))
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print("*", end= " ")
+#     print()
+
+# Pattern-10:
+# 1
+# 2 2
+# 3 3 3
+# 4 4 4 4
+# 5 5 5 5 5
+# 6 6 6 6 6 6
+# 7 7 7 7 7 7 7
+# 8 8 8 8 8 8 8 8
+# 9 9 9 9 9 9 9 9 9
+# 10 10 10 10 10 10 10 10 10 10
+
+# n = int(input("enter the number of row: "))
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print(i,end=" ")
+#     print()
+
+# Pattern-11:
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+# 1 2 3 4 5 6
+# 1 2 3 4 5 6 7
+# 1 2 3 4 5 6 7 8
+
+# n = int(input("enter the number of row: "))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+
+# Pattern-12:
+# A
+# B B
+# C C C
+# D D D D
+# E E E E E
+# F F F F F F
+# G G G G G G G
+# H H H H H H H H
+# I I I I I I I I I
+# J J J J J J J J J J
+
+# Pattern-14:
+# * * * * * * * * * *
+# * * * * * * * * *
+# * * * * * * * *
+# * * * * * * *
+# * * * * * *
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
+# n = int(input("enter the number of row: "))
+# for i in range(1,n+1):
+#     for j in range(n+1-i):
+#         print("*",end = " ")
+#     print()
+
+
+# Pattern-15:
+# 1 1 1 1 1 1 1 1 1 1
+# 2 2 2 2 2 2 2 2 2
+# 3 3 3 3 3 3 3 3
+# 4 4 4 4 4 4 4
+# 5 5 5 5 5 5
+# 6 6 6 6 6
+# 7 7 7 7
+# 8 8 8
+# 9 9
+# 10
+
+# n = int(input("enter the number of row: "))
+# for i in range(1,n+1):
+#     for j in range(n+1-i):
+#         print(i,end=" ")
+#     print()
+
+# Pattern-16:
+# 1 2 3 4 5 6 7 8 9 10
+# 1 2 3 4 5 6 7 8 9
+# 1 2 3 4 5 6 7 8
+# 1 2 3 4 5 6 7
+# 1 2 3 4 5 6
+# 1 2 3 4 5
+# 1 2 3 4
+# 1 2 3
+# 1 2
+# 1
+
+# n = int(input("enter the number of row: "))
+# for i in range(1,n+1):
+#     for j in range(1,n+2-i):
+#         print(j,end = " ")
+#     print()
+
+# Pattern-17:
+# A A A A A A A A A A
+# B B B B B B B B B
+# C C C C C C C C
+# D D D D D D D
+# E E E E E E
+# F F F F F
+# G G G G
+# H H H
+# I I
+# J
+
+# n =int(input("enter the number of row: "))
+# for i in range(1,n+1):
+#     for j in range(n+1-i):
+#         print(chr(64+i),end = " ")
+#     print()
+
+
+# Pattern-18:
+# A B C D E F G H I J
+# A B C D E F G H I
+# A B C D E F G H
+# A B C D E F G
+# A B C D E F
+# A B C D E
+# A B C D
+# A B C
+# A B
+# A
+
+# n = int(input("enter the number of row: "))
+# for i in range(1,n+1):
+#     for j in range(1,n+2-i):
+#         print(chr(64+j),end=" ")
+#     print()
+
+
+# Pattern-19:
+# 10 10 10 10 10 10 10 10 10 10
+# 9 9 9 9 9 9 9 9 9
+# 8 8 8 8 8 8 8 8
+# 7 7 7 7 7 7 7
+# 6 6 6 6 6 6
+# 5 5 5 5 5
+# 4 4 4 4
+# 3 3 3
+# 2 2
+# 1
+
+# n = int(input("enter the number of row: "))
+# for i in range(1,n+1):
+#     for j in range(n+1-i):
+#         print(n+1-i,end=" ")
+#     print()
+
+
+# Pattern-20:
+# 10 9 8 7 6 5 4 3 2 1
+# 10 9 8 7 6 5 4 3 2
+# 10 9 8 7 6 5 4 3
+# 10 9 8 7 6 5 4
+# 10 9 8 7 6 5
+# 10 9 8 7 6
+# 10 9 8 7
+# 10 9 8
+# 10 9
+# 10
+
+# n = int(input("enter the number of row: "))
+# for i in range(1,n+1):
+#     for j in range(1,n+2-i):
+#         print(n+1-j,end =" ")
+#     print()
+
+
+# Pattern-21:
+# J J J J J J J J J J
+# I I I I I I I I I
+# H H H H H H H H
+# G G G G G G G
+# F F F F F F
+# E E E E E
+# D D D D
+# C C C
+# B B
+# A
+
+# n = int(input("enter the number of row: "))
+# for i in range(1,n+1):
+#     for j in range(1,n+2-i):
+#         print(chr(65+n-i),end=" ")
+#     print()
+
+
+# Pattern-22:
+# J I H G F E D C B A
+# J I H G F E D C B
+# J I H G F E D C
+# J I H G F E D
+# J I H G F E
+# J I H G F
+# J I H G
+# J I H
+# J I
+# J
+
+# n = int(input("enter the number of row: "))
+# for i in range(1,n+1):
+#     for j in range(1,n+2-i):
+#         print(chr(65+n-j),end=" ")
+#     print()
+
+
+# # Pattern-23:
+ 
+#           *
+#          * *
+#         * * *
+#        * * * *
+#       * * * * *
+#      * * * * * *
+#     * * * * * * *
+#    * * * * * * * *
+#   * * * * * * * * *
+#  * * * * *  * * * * *
+
+n = int(input("enter the number of row: "))
+for i in range(1,n+1):
+    print(" "*(n-i)," *"*i,end=" ")
+    print()
+
