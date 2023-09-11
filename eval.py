8370099bfaeba6c27d033d1fb1c82cc2c2974a06
Shivani Chauhan <shivanichauhandev.@gmail.com>
2023-09-11 11:21:04 +0530

    added new file

---
+++ eval.py
@@ -0,0 +1,17 @@
+# x =eval(input("enter some data: "))
+# print(x)
+# print(type(x))
+
+a = eval(input("enter first value: "))
+b = eval(input("enter second value: "))
+# print(type(a) ,type(b))
+print(a + b)
+
+# a, b, c =[eval(x) for x in input("enter three values: ").split(",")]
+# print(type(a), type(b),type(c))
+
+# x = eval("20 % 3 + 40 / 2 + 60")
+# print(x)
+
+# a, b = 
+
