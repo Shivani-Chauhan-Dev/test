6e1c9da44e9d3fe95ed854404dcb465ab1d93624
Shivani Chauhan <shivanichauhandev.@gmail.com>
2023-09-11 11:03:52 +0530

    added new program file

---
+++ pythonvariable.py
@@ -0,0 +1,108 @@
+x = 345
+print(x)
+print(type(x))
+x = 3.68
+y = 5
+c = x+y
+print(x+y)
+print(type(c))
+light = "red"
+print(light)
+print(type(light))
+x = "shivani"
+y = "chauhan"
+c = x+y 
+print(c)
+x = 5
+y = "chauhan"
+print(x)
+print(y)
+x = 6
+x = "chauhan"
+print(x)
+x = float(3)
+y = int(6)
+z = complex(6)
+a = str(6)
+print(x)
+print(y)
+print(z)
+print(a)
+myvariable = "jhon"
+print(myvariable)
+print(type(myvariable))
+myvariable_name = "jhon"
+print(myvariable_name)
+x,y,z,a = "orange" ,"apple" ,"mango" ,6 
+print(x)
+print(y)
+print(z)
+print(a)
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+x=y=u=z=e=s="shivani"
+print(x)
+print(y)
+print(u)
+print(z)
+print(e)
+print(s)
+print(type(x))
+num = [6,7,8,9]
+x,y,z,a= num
+print(x)
+print(y)
+print(z)
+print(a)
+print(x+y)
+x = "my name is shivani chauhan"
+print(x)
+x = "my name"
+y = "is shivani"
+z = "chauhan"
+print(x,y,z)
+
+print(x,y,z)
+print(x+y+z)
+x = 0.36
+y = 6+2j
+z = 6
+a = "chauhan"
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+x = 2e11
+print(type(x))
+x = 5 #int
+
+z = 3.66 #float
+a = complex(x)
+
+c = int(z)
+print(a)
+
+print(c)
+print(type(a))
+print(type(c))
+sky = "blue"
+print(sky)
+print(type(sky))
+d ={10:"learn", 12: "advise"}
+print(d[10])
+print(d[12])
+x= "hello\n world\n myname\n is\n shivani\n chauhan"
+print(x)
+x = """what is your name ? my name is shivani chauhan.in 
+which class do u read
+i read in first class
+"""
+print(x)
+x = "hello , world"
+print(x[1])
+print(x[0])
+print(x[5])
+print(x[6])
+print(len(x))
