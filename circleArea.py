3d8f907f901fb2b4d90b085919c5d038130d9f40
Shivani Chauhan <shivanichauhandev.@gmail.com>
2023-09-11 11:14:02 +0530

    added new file

---
+++ circleArea.py
@@ -0,0 +1,70 @@
+# from math import pi,sqrt
+# print(pi*sqrt(int(input("enter the number: "))))
+# print(pi*sqrt(4))
+
+
+# a,b = [float(x) for x in input("enter 2 float values").split(",")]
+# print(a + b)
+
+
+# s,b = int(input("entre 1st integer values: ")), int(input("enter 2nd integer value: "))
+# print(s + b)
+
+# s = input("entre 1st integer values: ")
+# s2 = s.split(",")
+# a = int(s2[0])
+# b = int(s2[1])
+# print(s2)
+# print(type(a), a)
+
+# print(type(b), b)
+# print(a + b)
+
+# a = int(input("enter first integer value: "))
+# b = int(input("enter second integer value: "))
+# c = int(input("enter third integer value: "))
+# print(a + b + c)
+# print(type(a), type(b), type(c))
+
+# a, b, c = int(input("enter first integer value")),int(input("enter second integer value")),int(input("enter third integer value"))
+
+# print(a + b + c)
+
+# b =int(input("enter first integer value: "))
+# c = int(input("enter second integer value: "))
+# print(b - c)
+# print(type(b), type(c))
+
+# b, c = int(input("enter first integer value: ")), int(input("enter second integer value: "))
+# print(b - c)
+
+# a = input("enter float value: ")
+# a2 = a.split()
+# b = float(a2[0])
+# c = float(a2[1])
+# print(a2)
+# print(type(a2))
+# print(type(b),b , type(c),c)
+# print(b + c)
+
+# a,b = [float(x) for x in input("enter float value: ").split()]
+# print(a + b)
+
+# a = float(input("enter first value: "))
+# b =float(input("enter second value: "))
+# print(a/b)
+# print(type(a),type(b))
+
+# a,b = (float(input("enter first value: ")),float(input("enter second value: ")))
+# print(a/b)
+
+
+# a = complex(input("enter first integer value: "))
+# b =complex(input("enter second integer value: "))
+# print(a * b)
+
+# print(complex(input("enter first value: ")) * complex(input("enter second value: ")))
+
+
+# a = input("enter 2 integer value: ")
+
